f07a294e59c35b9fa64f54782baa4ce2e0613c0b
BrunoDS <bruno.diego.x@hotmail.com>
2023-04-02 05:31:39 +0000

    add default colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6A0DAD",
+        secondary: "#C7A4FF",
+        tertiary: "#FFD700",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.jpg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
