c2729c77a23306d964f080eede63c307f3d9bec5
BrunoDS <bruno.diego.x@hotmail.com>
2023-04-02 05:25:19 +0000

    Add readme and 3d models folder

---
+++ README.md
@@ -0,0 +1,7 @@
+![Logo](#)
+
+Welcome fellow visitor!
+
+Feel free to access the [live website](https://bruno-diego.github.io/brunodiegodev/) (CTRL + Click to open in a new tab).
+
+---
---
+++ src/assets/index.js
@@ -0,0 +1,58 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import ecommerce from "./ecommerce.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import aws from "./company/aws-icon.png";
+import www from "./company/wwwicon.png";
+
+import copacabana from "./copacabana.png";
+import bdcrypto from "./bdcrypto.png";
+import snakegame from "./snakegame.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  ecommerce,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  aws,
+  www,
+  copacabana,
+  bdcrypto,
+  snakegame,
+};
