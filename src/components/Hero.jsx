8eeda351e9f830b454e86c71b029c3417fce5876
BrunoDS <bruno.diego.x@hotmail.com>
2023-04-16 18:22:03 +0000

    Create a button to go to about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,6 +31,27 @@ const Hero = () => {
                     <GlobeCanvas />
                 </motion.div>
             </div>
+            <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center gap-5 z-50 px-5 py-5'>
+                {/* Make the message stand out */}
+                <a href='#about'>
+                    <p className='flex flex-row justify-center items-center gap-5 mb-2'>
+                        <span className='text-white text-xl uppercase font-bold cursor-pointer ease-in hover:scale-110 duration-300 transition-all hover:rounded-lg hover:outline hover:outline-4 hover:outline-white outline-none'>Explore</span>
+                    </p>
+                    <div className='w-[35px] h-[64px]  flex justify-center items-start p-2 mx-auto'>
+                        <motion.div
+                            animate={{
+                                y: [0, 24, 0],
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                            }}
+                            className='w-3 h-3 rounded-full bg-white mb-1'
+                        />
+                    </div>
+                </a>
+            </div>
         </section >
     )
 }
