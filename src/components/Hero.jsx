1c72c8106a3daa29efe5f4c1472619157653db8f
BrunoDS <bruno.diego.x@hotmail.com>
2023-06-29 22:35:26 +0000

    Added cookies box

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,17 @@ const Hero = () => {
                     </p>
                 </div>
             </div>
+            <section className="cookieSec fixed max-w-md p-4 mx-auto bg-white border border-gray-200 dark:bg-gray-800 left-12 bottom-16 dark:border-gray-700 rounded-2xl z-50">
+                <h2 className="font-semibold text-gray-800 dark:text-white">🍪 Cookie Notice</h2>
+
+                <p className="mt-4 text-sm text-gray-600 dark:text-gray-300">We use cookies to ensure that we give you the best experience on our website. <a href="https://commission.europa.eu/cookies-policy_en" className="text-blue-500 hover:underline" target="_blank">Read cookies policies</a>. </p>
+                
+                <div className="wrapper flex items-center justify-between mt-4 gap-x-4 shrink-0">
+                    <button className="button text-xs bg-gray-900 font-medium rounded-lg hover:bg-gray-700 text-white px-4 py-2.5 duration-300 transition-colors focus:outline-none">
+                        Accept
+                    </button>
+                </div>
+            </section>
             <div className={`flex grow justify-center overflow-hidden h-full absolute inset-x-0 top-2/4`}>
                 <motion.div initial="hidden" animate="show" variants={slideIn("up", "", 1, 6)}>
                     <GlobeCanvas />
